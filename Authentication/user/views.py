2c87349d49e445cf83bc3ff94a9722f15132d9b4
Kisekka David <genza@MacBook-Pro-6.local>
2019-03-21 16:03:57 +0300

    Create UserRegistration route
    - Add userregistration view method in the views
    - Add routes that map to the userregistartion view
    
    [Finishes #164778316]

--- Authentication/user/views.py
+++ Authentication/user/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterUser(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        email = request.data.get("email", "")
+
+        if not username  and not password and not email:
+            return Response(
+                data = {
+                    "error": "Missing Information"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = User.objects.create_user(username=username, password=password, email=email)
+        return Response(
+            data={
+                "Username": username,
+                "Password": password,
+                "email": email
+            },
+            status=status.HTTP_201_CREATED
+
+        )
+
+        
+
+
+
